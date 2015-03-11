d44bc77037977a819f6bc64e24287e2b9a12290d
xiao.hu <xiao.hu@ck-telecom.com>
2015-03-11 15:19:02 +0800

    update contentprovider

--- src/com/goldsand/outdoor/HomeView.java
+++ src/com/goldsand/outdoor/HomeView.java
@@ -2,8 +2,11 @@ package com.goldsand.outdoor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class HomeView extends Activity{
+public class HomeView extends Activity implements OnClickListener{
+    
     
     @Override
     protected void onCreate(Bundle savedInstanceState) 
@@ -11,4 +14,11 @@ public class HomeView extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.outdoor);
     }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
