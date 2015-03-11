02191056c5da7b8bc656d788f55f3eebb1a9cbc3
xiao.hu <xiao.hu@ck-telecom.com>
2015-03-11 15:01:03 +0800

    add the main activity

---
+++ src/com/goldsand/outdoor/HomeView.java
@@ -0,0 +1,14 @@
+package com.goldsand.outdoor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeView extends Activity{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.outdoor);
+    }
+}
